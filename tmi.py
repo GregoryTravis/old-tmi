b4c9bdf412c31a4f1dd58c17a29a488d3a3e5bf8
Gregory Travis <greg.m.travis@gmail.com>
2017-02-23 19:32:33 -0500

    srecqeq, ceq, pnot, where

--- tmi.py
+++ tmi.py
@@ -5,8 +5,8 @@ def d(**kwargs):
   return dict(**kwargs)
 
 def ss(rel):
-  fields = rel[0].keys() if len(rel) > 0 else ['']
-  return tabulate([[rec[f] for f in fields] for rec in rel], fields, tablefmt='fancy_grid')
+  cols = rel[0].keys() if len(rel) > 0 else ['']
+  return tabulate([[rec[f] for f in cols] for rec in rel], cols, tablefmt='fancy_grid')
 assert ss([d(a=1, b=2), d(a=10, b=20)]) == u'╒═════╤═════╕\n│   a │   b │\n╞═════╪═════╡\n│   1 │   2 │\n├─────┼─────┤\n│  10 │  20 │\n╘═════╧═════╛'
 
 def s(*args):
@@ -18,4 +18,29 @@ r = [
   d(a=1, b=2),
   d(a=10, b=20),
 ]
-s(r, [])
+
+# True if equal to srec on all fields of srec
+def sreceq(srec): return lambda rec: all([rec[col] == srec[col] for col in srec.keys()])
+
+assert sreceq(d(a=1))(d(a=1, b=2))
+assert sreceq(d(a=1))(d(a=1, b=2, c=3))
+assert sreceq(d(c=3))(d(a=1, b=2, c=3))
+assert not sreceq(d(a=2))(d(a=1, b=2))
+
+def ceq(col, value): return sreceq({ col: value })
+assert ceq('a', 1)(d(a=1, b=2))
+assert ceq('a', 1)(d(a=1, b=2, c=3))
+assert ceq('c', 3)(d(a=1, b=2, c=3))
+assert not ceq('a', 2)(d(a=1, b=2))
+
+def pnot(pred): return lambda x: not pred(x)
+assert not pnot(ceq('a', 1))(d(a=1, b=2))
+assert not pnot(ceq('a', 1))(d(a=1, b=2, c=3))
+assert not pnot(ceq('c', 3))(d(a=1, b=2, c=3))
+assert pnot(ceq('a', 2))(d(a=1, b=2))
+
+def where(rel, pred):
+  return [rec for rec in rel if pred(rec)]
+assert [d(a=1, b=2)] == where(r, ceq('a', 1))
+assert [d(a=10, b=20)] == where(r, pnot(ceq('a', 1)))
+s(where(r, pnot(ceq('a', 1))))
