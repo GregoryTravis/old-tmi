894fe5342f8405ecc777466aa6afe195b02ddc19
Gregory Travis <greg.m.travis@gmail.com>
2017-03-04 17:27:43 -0500

    oldy join

--- old.py
+++ old.py
@@ -2,7 +2,12 @@ from tmi import *
 from web import call
 
 def foo(a, b):
-  return ss(Deref(File('old.dat'), 'player'))
+  db = File('old.dat')
+  player = Deref(db, 'player')
+  card = Deref(db, 'card')
+  game = Deref(db, 'game')
+  hand = Deref(db, 'hand')
+  return ss(Join(game, Join(player, Join(card, hand))))
 
 def main():
   return 'OLDMAIN ' + call('fuu', foo, 'xx', 'yy')
