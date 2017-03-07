fd3acd44ce4eab860f83b7dd82be5a3c71348bb7
Gregory Travis <greg.m.travis@gmail.com>
2017-03-07 10:29:26 -0500

    oops

---
+++ io.py
@@ -0,0 +1,10 @@
+import ast
+import pprint
+
+def readdat(filename):
+  with open(filename) as f:
+    return ast.literal_eval(f.read())
+
+def writedat(filename, data):
+  with open(filename, 'w') as f:
+    f.write(pprint.pformat(data))
