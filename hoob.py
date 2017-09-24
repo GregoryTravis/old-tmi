e2a353ea061365297a1d8ad493bc708371abb8c3
Gregory Travis <greg.m.travis@gmail.com>
2017-09-24 14:28:43 -0400

    trying tmi grammar, not working, more calls to ya_one

--- hoob.py
+++ hoob.py
@@ -1,5 +1,6 @@
 from collections import defaultdict
 from lib import *
+from pprint import pprint
 import sys
 
 def flat1(os):
@@ -26,7 +27,7 @@ gram = [
 
 #@trace
 def map_also(f, os):
-  haha = map(f, os)
+  #haha = map(f, os)
   #print 'uM', haha, zip(*haha), map(list, zip(*haha))
   return map(list, zip(*map(f, os)))
 #print map_also(lambda x: (x*2, 'hey'+str(x)), [1, 2, 3])
@@ -42,6 +43,7 @@ def new_sym():
 def reduce_long_opt(opt):
   if len(opt) > 2:
     nu = new_sym()
+    #print 'wel', nu, opt[0:2]
     return [[nu] + opt[2:], [[nu, [[opt[0], opt[1]]]]]]
   else:
     return [opt, []]
@@ -50,7 +52,7 @@ def reduce_long_opt(opt):
 def preprocess1(gram):
   newgram = []
   for rule in gram:
-    print 'r', rule
+    #print 'r', rule
     nt, opts = rule
     newopts, more_rules = map_also(reduce_long_opt, opts)
     newgram = newgram + [[nt, newopts]] + flat1(more_rules)
@@ -108,13 +110,22 @@ def ya_all(os):
   if len(os) == 1:
     return ya_one(os[0])
   elif len(os) == 2:
-    return ya_two(os[0], os[1])
+    #return ya_two(os[0], os[1])
+    return flat1([ya_two(x, y) for x in ya_one(os[0]) for y in ya_one(os[1])])
   else:
     a, d = os[0], os[1:]
     a_nts = ya_one(a)
     d_nts = ya_all(d)
     #print os, a_nts, d_nts
-    type_one = [a for b in [ya_two(a, b) for a in a_nts for b in d_nts] for a in b]
+    #type_one = [a for b in [ya_two(a, b) for a in a_nts for b in d_nts] for a in b]
+    #vv = ya_one(a_nts)
+    #vv2 = ya_one(d_nts)
+    #print 'HEY', vv, vv2
+
+    #type_one = flat1([ya_two(x, y) for x in a_nts for y in d_nts])
+    #type_one = flat1([ya_two(x, y) for x in ya_one(a_nts) for y in ya_one(d_nts)])
+    type_one = flat1([ya_two(x, y) for x in flat1(map(ya_one, a_nts)) for y in flat1(map(ya_one, d_nts))])
+
     ad = os[1]
     dd = os[2:]
     """
@@ -122,8 +133,60 @@ def ya_all(os):
     dd_nts = ya_all(dd)
     type_two = [a for b in [ya_two(a, b) for a in a_ad_nts for b in dd_nts] for a in b]
     """
-    a_ad_nts = ya_two(a, ad)
-    type_two = flat1([ya_all([nt] + dd) for nt in a_ad_nts])
-    return type_one + type_two
+    #print 'TTWO', os, a, ad
+    haha = [[x, y, ya_two(x, y)] for x in ya_one(a) for y in ya_one(ad)]
+    #print 'AAA', haha
+    a_ad_nts = flat1([ya_two(x, y) for x in ya_one(a) for y in ya_one(ad)])
+    #a_ad_nts = ya_two(a, ad)
+    type_two = flat1([ya_all([nt] + dd) for nt in flat1(map(ya_one, a_ad_nts))])
+    return flat1(map(ya_one, (type_one + type_two)))
+
+#print ya_all(tokens)
+
+"""
+start = decls $;
+
+decls = definition ';' decls | definition;
+
+definition = defpat: {identifier}+ '=' body: exp;
+
+exp = let | binopapp | app | identifier | integer;
+
+let = 'let' '{' decls: decls '}' 'in' body: exp;
+
+binopapp = left: identifier op: operator right: exp;
+
+app = identifier {identifier}+;
+"""
+
+gram = [
+  ['start', [['decls']]],
+  ['decls', [['definition', 'semicolon', 'decls'], ['definition']]],
+  ['definition', [['identifier_plus', 'equals', 'exp']]],
+  ['identifier_plus', [['identifier_plus', 'identifier'], ['identifier']]],
+  ['exp', [['let'], ['binopapp'], ['app'], ['identifier'], ['integer']]],
+  ['let', [['let_keyword', 'lcb', 'decls', 'rcb', 'in_keyword', 'exp']]],
+  ['binopapp', [['exp', 'operator', 'exp']]],
+  ['app', [['identifier', 'identifier_plus']]],
+]
+
+#foo a b = let r b = b + b
+           #in r
+
+tokens = [ 'identifier', 'identifier', 'identifier', 'equals', 'let_keyword', 'lcb', 'identifier',
+'identifier', 'equals', 'identifier', 'operator', 'identifier', 'rcb', 'in_keyword', 'identifier' ]
+
+tokens = [ 'identifier', 'identifier', 'equals', 'identifier', 'identifier' ]
+
+tokens = [ 'let_keyword', 'lcb', 'identifier', 'identifier', 'equals', 'identifier', 'identifier', 'rcb', 'in_keyword', 'exp' ]
+
+ogram = gram
+gram = preprocess(gram)
+print sh_grammar(ogram)
+print sh_grammar(gram)
+
+build_maps(gram)
+pprint(dict(one_to_nts))
+pprint(dict(two_to_nts))
 
 print ya_all(tokens)
